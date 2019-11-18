594ae00724082549715503847ae26b3c451b3a19
Logan Karney <karneyl@mail.gvsu>
2019-11-18 15:48:03 -0500

    Commented out ref code

--- granddrive/src/index.js
+++ granddrive/src/index.js
@@ -7,12 +7,13 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-AppDB.ref("Users")
+/*AppDB.ref("Users")
   .push()
   .set({ username: "Logan" });
 AppDB.ref("Documents")
   .push()
   .set({ name: "test", ownerId: "-Lu-ErjNSuKRgZedW7qu" });
+  */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
