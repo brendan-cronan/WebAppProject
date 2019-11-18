4cc8e4ab1d41179ec51b2a7819ac56288073a5d2
Logan Karney <karneyl@mail.gvsu>
2019-11-18 15:46:24 -0500

    Added db ref

--- granddrive/src/index.js
+++ granddrive/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { AppDB } from "./db-init.js";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+AppDB.ref("Users")
+  .push()
+  .set({ username: "Logan" });
+AppDB.ref("Documents")
+  .push()
+  .set({ name: "test", ownerId: "-Lu-ErjNSuKRgZedW7qu" });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
